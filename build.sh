51ebb76470f0ded3532a8c2438a6650f98a5ff4b
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 13:02:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
 pacman -Syu archiso git --noconfirm
 
 mkdir /repo
-useradd -m aur-temp
+useradd -m aurtemp
 
-runuser -l aur-temp -c git clone https://aur.archlinux.org/yay-bin
+runuser -l aurtemp -c "git clone https://aur.archlinux.org/yay-bin"
 cd yay-bin
 source PKGBUILD && pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}"
-runuser -l aur-temp -c makepkg
+runuser -l aurtemp -c makepkg
 cp *.pkg.tar.zst /repo
 cd ..
 rm -rf yay-bin 
 
-runuser -l aur-temp -c git clone https://aur.archlinux.org/plymouth
+runuser -l aurtemp -c "git clone https://aur.archlinux.org/plymouth"
 cd plymouth
 source PKGBUILD && pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}
-runuser -l aur-temp -c makepkg
+runuser -l aurtemp -c makepkg
 cp *.pkg.tar.zst /repo
 cd ..
 rm -rf plymouth
 
-runuser -l aur-temp -c git clone https://aur.archlinux.org/gdm-plymouth
+runuser -l aurtemp -c "git clone https://aur.archlinux.org/gdm-plymouth"
 cd gdm-plymouth
 source PKGBUILD && pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}
-runuser -l aur-temp -c makepkg
+runuser -l aurtemp -c makepkg
 cp *.pkg.tar.zst /repo
 cd ..
 rm -rf gdm-plymouth
 
 repo-add /repo/custom.db.tar.gz /repo/*
-mkarchiso -v -w /archiso -o /build archFiles
+mkarchiso -v -w /archiso -o /build /home/root/archFiles
