29e42e09b0980fe2dba2d4e3ad383441308ce302
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 15:15:51 +0100

    update

--- build.sh
+++ build.sh
@@ -12,19 +12,19 @@ runuser -u maketmp -- cp -r ./archFiles/* /workingDir/archFiles/
 cd /workingDir/aurPackages
 
 # Aur packages clone, makepkg and add to repo
-if pacman -Qi yay-bin &>/dev/null; then YayInstalled=true; else YayInstalled=false; fi
+if pacman -Qi yay-bin &>/dev/null; then YayInstalled=1; else YayInstalled=0; fi
 runuser -u maketmp -- git clone https://aur.archlinux.org/yay-bin
 cd yay-bin 
 runuser -u maketmp -- makepkg -si --noconfirm
 cp *.pkg.tar.zst /repo
 
-if pacman -Qi yay-bin &>/dev/null; then PlymouthInstalled=true; else PlymouthInstalled=false; fi
+if pacman -Qi yay-bin &>/dev/null; then PlymouthInstalled=1; else PlymouthInstalled=0; fi
 runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
 cd plymouth
 runuser -u maketmp -- makepkg -si --noconfirm
 cp *.pkg.tar.zst /repo
 
-if pacman -Qi libgdm-plymouth &>/dev/null; then GDMPlymouthInstalled=true; else GDMPlymouthInstalled=false; fi
+if pacman -Qi libgdm-plymouth &>/dev/null; then GDMPlymouthInstalled=1; else GDMPlymouthInstalled=0; fi
 runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
 cd gdm-plymouth
 runuser -u maketmp -- makepkg -si --noconfirm
@@ -34,9 +34,9 @@ repo-add /repo/custom.db.tar.gz /repo/*
 
 if [ $YayInstalled -eq 0 ]; then pacman -Rsnc yay-bin --noconfirm; fi
 if [ $PlymouthInstalled -eq 0 ]; then pacman -Rsnc plymouth --noconfirm; fi
-if [ $GDMPlymouthInstalled -eq 0 ]; then pacman -Rsnc libgdm-plymouth --noconfirm; fi
+if [ $GDMPlymouthInstalled -eq 0 ]; then pacman -Rsnc gdm-plymouth --noconfirm; fi
 
-# Make iso
+# Make ISO
 mkarchiso -v -w /archiso -o /lukeMechArch /workingDir/archFiles
 
 # Delete temp user and dirs
