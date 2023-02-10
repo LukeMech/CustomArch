5d0b20dfe1af916e5b3a1277ae685f483edaf131
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 18:28:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-set -e
 
+# Start config
+set -e
+if pacman -Qi archiso &>/dev/null; then ArchisoInstalled=1; else ArchisoInstalled=0; fi
+if pacman -Qi git &>/dev/null; then GitInstalled=1; else GitInstalled=0; fi
 pacman -Syu git base-devel archiso --noconfirm
 
 # Create temp user and working dir
@@ -13,12 +16,15 @@ runuser -u maketmp -- cp -r ./archFiles/* /workingDir/archFiles/
 cd /workingDir/aurPackages
 
 # Aur packages clone, makepkg and add to repo
+
+# Yay
 runuser -u maketmp -- git clone https://aur.archlinux.org/yay
 cd yay
 runuser -u maketmp -- makepkg -s --noconfirm
 cp *.pkg.tar.zst /repo
 cd ..
 
+# Plymouth
 if pacman -Qi plymouth &>/dev/null; then PlymouthInstalled=1; else PlymouthInstalled=0; fi
 runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
 cd plymouth
@@ -26,6 +32,7 @@ runuser -u maketmp -- makepkg -si --noconfirm
 cp *.pkg.tar.zst /repo
 cd ..
 
+# Gnome Display Manager - plymouth
 runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
 cd gdm-plymouth
 runuser -u maketmp -- makepkg -s --noconfirm
@@ -34,12 +41,13 @@ cd ..
 
 repo-add /repo/aur-local.db.tar.gz /repo/*
 
-if [ $PlymouthInstalled -eq 0 ]; then pacman -Rns plymouth --noconfirm; fi
-pacman -Qdtq | pacman -Rns - --noconfirm
-
 # Make ISO
 mkarchiso -v -w /archiso -o /lukeMechArch /workingDir/archFiles
 
-# Delete temp user and dirs
+# Delete temp user, dirs and cleanup packages
 rm -rf /repo &>/dev/null
 userdel -r maketmp &>/dev/null
+if [ $ArchisoInstalled -eq 0 ]; then pacman -Rns archiso --noconfirm; fi
+if [ $GitInstalled -eq 0 ]; then pacman -Rns git --noconfirm; fi
+if [ $PlymouthInstalled -eq 0 ]; then pacman -Rns plymouth --noconfirm; fi
+pacman -Qdtq | pacman -Rns - --noconfirm
