0f88503ef16c0da18ffa97f59a4db4fd7f10a8f4
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 13:55:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,22 +2,21 @@ pacman -Syu archiso git base-devel --noconfirm
 mkdir /repo
 
 useradd -m -d /maketmp maketmp
-
-runuser -u maketmp -- "git clone https://aur.archlinux.org/yay-bin" 
+runuser -u maketmp -- git clone https://aur.archlinux.org/yay-bin
 ls
+runuser -u maketmp -- ls
 cd yay-bin 
-runuser -u maketmp makepkg -s
-runuser -u maketmp ls
+runuser -u maketmp -- makepkg -s
 cp *.pkg.tar.zst /repo
 
-runuser -u maketmp git "clone https://aur.archlinux.org/plymouth"
+runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
 cd plymouth
-runuser -u maketmp makepkg -s
+runuser -u maketmp -- makepkg -s
 cp *.pkg.tar.zst /repo
 
-git clone https://aur.archlinux.org/gdm-plymouth
+runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
 cd gdm-plymouth
-runuser -u maketmp "makepkg -s"
+runuser -u maketmp -- makepkg -s
 cp *.pkg.tar.zst /repo
 
 userdel maketmp
