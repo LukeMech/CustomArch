686054b516fa93b7342540c086ad422232aa1ecc
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 12:37:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,25 @@
 mkdir /repo
-cp ./archFiles/packages/* /repo
+
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg
+cp *.pkg.tar.zst /repo
+cd ..
+rm -rf yay-bin 
+
+git clone https://aur.archlinux.org/plymouth
+cd ../plymouth
+makepkg
+cp *.pkg.tar.zst /repo
+cd ..
+rm -rf plymouth
+
+git clone https://aur.archlinux.org/gdm-plymouth
+cd ../gdm-plymouth
+makepkg
+cp *.pkg.tar.zst /repo
+cd ..
+rm -rf gdm-plymouth
+
 repo-add /repo/custom.db.tar.gz /repo/*
 mkarchiso -v -w /archiso -o /build archFiles
