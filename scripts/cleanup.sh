da07ad64f6f2a6e9c4bb9f6c6a2b23833bb57747
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 22:32:55 +0100

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,19 +2,19 @@
 set -e
 
 read -p '=> [QUESTION] Delete temporary user and build files? [Y/n] ' deleteuser
-if [ "$deleteuser" = 'n' ]; then echo "Not deleting."; else echo "Deleting 'maketmp' user and '/workingDir' path..." && userdel -r maketmp; fi
+if [ "$deleteuser" = 'n' ]; then echo "Not deleting."; else echo "Deleting 'maketmp' user and '/workingDir' path..." && userdel -r maketmp &>/dev/null || true; fi
 
 read -p '=> [QUESTION] Delete temporary repo with aur packages? [Y/n] ' deleterepo
-if [ "$deleterepo" = 'n' ]; then echo "Not deleting."; else echo "Deleting '/repo'..." && rm -rf /repo; fi
+if [ "$deleterepo" = 'n' ]; then echo "Not deleting."; else echo "Deleting '/repo'..." && rm -rf /repo &>/dev/null || true; fi
 
 read -p '=> [QUESTION] Uninstall archiso? (No longer required for build) [Y/n] ' uninstallarchiso
-if [ "$uninstallarchiso" = 'n' ]; then echo "Not removing."; else pacman -Rns archiso --noconfirm; fi
+if [ "$uninstallarchiso" = 'n' ]; then echo "Not removing."; else pacman -Rns archiso --noconfirm || true; fi
 
 read -p '=> [QUESTION] Uninstall plymouth? (No longer required for build) [y/N] ' uninstallplymouth
-if [ "$uninstallplymouth" = 'y' ]; then pacman -Rns plymouth --noconfirm; else echo "Not removing."; fi
+if [ "$uninstallplymouth" = 'y' ]; then pacman -Rns plymouth --noconfirm || true; else echo "Not removing."; fi
 
 read -p '=> [QUESTION] Uninstall git? (No longer required for build) [y/N] ' uninstallgit
-if [ "$uninstallgit" = 'y' ]; then pacman -Rns git --noconfirm; else echo "Not removing."; fi
+if [ "$uninstallgit" = 'y' ]; then pacman -Rns git --noconfirm || true; else echo "Not removing."; fi
 
 echo "=> [INFO] Removing unneeded packages..."
 pacman -Qdtq | pacman -Rns - --noconfirm
