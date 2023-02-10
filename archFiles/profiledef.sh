bb4b6161c25aceb1ef118ed8822ac27fbde930bc
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 22:22:10 +0100

    hmm

--- archFiles/profiledef.sh
+++ archFiles/profiledef.sh
@@ -8,7 +8,8 @@ iso_application="LukeMechArch Live CD"
 iso_version="$(date +%y.%m.%d))"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('uefi-x64.systemd-boot.esp')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
