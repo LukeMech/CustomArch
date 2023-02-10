cb80712b90678185f85ad1fd52d03b90c644e6d0
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-10 14:36:03 +0100

    Update downloadAur.sh

--- downloadAur.sh
+++ downloadAur.sh
@@ -11,17 +11,17 @@ cd /maketmp
 
 runuser -u maketmp -- git clone https://aur.archlinux.org/yay-bin
 cd yay-bin 
-runuser -u maketmp -- makepkg -s
+runuser -u maketmp -- makepkg -s --noconfirm
 cp *.pkg.tar.zst /repo
 
 runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
 cd plymouth
-runuser -u maketmp -- makepkg -s
+runuser -u maketmp -- makepkg -s --noconfirm
 cp *.pkg.tar.zst /repo
 
 runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
 cd gdm-plymouth
-runuser -u maketmp -- makepkg -s
+runuser -u maketmp -- makepkg -s --noconfirm
 cp *.pkg.tar.zst /repo
 
 userdel maketmp
