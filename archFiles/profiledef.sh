f857504be97b15fb76b2eb1727ea404ef5a94d2c
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-11 12:13:54 +0100

    Update profiledef.sh

--- archFiles/profiledef.sh
+++ archFiles/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="LukeMechArch"
+iso_name="lukemech"
 iso_label="LukeMechArch_$(date +%y.%m)"
 iso_publisher="LukeMech <https://LukeMech.github.io>"
 iso_application="LukeMechArch Live CD"
