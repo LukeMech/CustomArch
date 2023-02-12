77e4da864630ac66b92a1bd523503cc9de40d3fc
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-12 12:21:54 +0100

    hmm

--- scripts/pullFromAur.sh
+++ scripts/pullFromAur.sh
@@ -18,38 +18,38 @@ runuser -u maketmp -- mkdir /workingDir/aurPackages
 runuser -u maketmp -- cp -r ./archFiles/* /workingDir/archFiles/
 cd /workingDir/aurPackages
 
-# # Aur packages - clone, make
-# echo "=> [INFO] Cloning and building packages..."
+# Aur packages - clone, make
+echo "=> [INFO] Cloning and building packages..."
 
-# # Pamac
-# runuser -u maketmp -- git clone https://aur.archlinux.org/archlinux-appstream-data-pamac
-# cd archlinux-appstream-data-pamac
-# runuser -u maketmp -- makepkg -si --noconfirm
-# cp *.pkg.tar.zst /repo
-# cd ..
-# runuser -u maketmp -- git clone https://aur.archlinux.org/libpamac-nosnap
-# cd libpamac-nosnap
-# runuser -u maketmp -- makepkg -si --noconfirm
-# cp *.pkg.tar.zst /repo
-# cd ..
-# runuser -u maketmp -- git clone https://aur.archlinux.org/pamac-nosnap
-# cd pamac-nosnap
-# runuser -u maketmp -- makepkg -s --noconfirm
-# cp *.pkg.tar.zst /repo
-# cd ..
+# Pamac
+runuser -u maketmp -- git clone https://aur.archlinux.org/archlinux-appstream-data-pamac
+cd archlinux-appstream-data-pamac
+runuser -u maketmp -- makepkg -si --noconfirm
+cp *.pkg.tar.zst /repo
+cd ..
+runuser -u maketmp -- git clone https://aur.archlinux.org/libpamac-nosnap
+cd libpamac-nosnap
+runuser -u maketmp -- makepkg -si --noconfirm
+cp *.pkg.tar.zst /repo
+cd ..
+runuser -u maketmp -- git clone https://aur.archlinux.org/pamac-nosnap
+cd pamac-nosnap
+runuser -u maketmp -- makepkg -s --noconfirm
+cp *.pkg.tar.zst /repo
+cd ..
 
-# # Plymouth
-# runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
-# cd plymouth
-# runuser -u maketmp -- makepkg -si --noconfirm
-# cp *.pkg.tar.zst /repo
-# cd ..
-# runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
-# cd gdm-plymouth
-# runuser -u maketmp -- makepkg -s --noconfirm
-# cp *.pkg.tar.zst /repo
-# cd ..
+# Plymouth
+runuser -u maketmp -- git clone https://aur.archlinux.org/plymouth
+cd plymouth
+runuser -u maketmp -- makepkg -si --noconfirm
+cp *.pkg.tar.zst /repo
+cd ..
+runuser -u maketmp -- git clone https://aur.archlinux.org/gdm-plymouth
+cd gdm-plymouth
+runuser -u maketmp -- makepkg -s --noconfirm
+cp *.pkg.tar.zst /repo
+cd ..
 
-# # Aur packages - add to repo
-# echo "=> [INFO] Creating local repo..."
-# repo-add /repo/aur-local.db.tar.gz /repo/*
+# Aur packages - add to repo
+echo "=> [INFO] Creating local repo..."
+repo-add /repo/aur-local.db.tar.gz /repo/*
