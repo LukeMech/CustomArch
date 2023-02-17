ab06893b393e962702aa82003673d44af4236e28
LukeMech <81306360+LukeMech@users.noreply.github.com>
2023-02-18 00:41:49 +0100

    Update pullFromAur.sh

--- scripts/pullFromAur.sh
+++ scripts/pullFromAur.sh
@@ -23,7 +23,7 @@ echo "=> [INFO] Cloning and building packages..."
 
 # Ping-Pong_Counter app
 wget https://github.com/LukeMech/Ping-Pong_Counter/releases/latest/download/PingPongCounter-x64.pacman
-cp Ping-pong_counter-x64.pacman /repo/ping-pong-counter.pkg.xz
+cp PingPongCounter-x64.pacman /repo/ping-pong-counter.pkg.xz
 
 # Emojis
 runuser -u maketmp -- git clone https://aur.archlinux.org/ttf-twemoji
